7a612d4b8c69916f2b9b3274a1c08d5e6153cb9f
ragulasneha <snehanitha2893@gmail.com>
2020-08-19 21:47:46 +0530

    i have changed my code to helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,16 +1,8 @@
-import java.util.Scanner;
-public class Temperature {
-	public final double TEMP=100.00;
+public class HelloWorld {
+	
 	public static void main (String [] args) {
 		
-		System.out.println("Enter current temperature");
-		Scanner keyboard = new Scanner(System.in);
-		double temp = keyboard.nextDouble();
-		if(temp<TEMP) {
-	        System.out.println("low temperature");
-		}else{
-			System.out.println("High Temperature");
-        	}
+		System.out.println("Hello World");
 	}
 
 }
